b30afda625cc786fea324201a99921cfd9e036f0
chang.ray <misterchangray@hotmail.com>
2023-03-21 17:52:35 +0800

    compiler 2

--- nand2tetris/projects/06/Compiler.py
+++ nand2tetris/projects/06/Compiler.py
@@ -165,7 +165,8 @@ class SymbolTable:
     self.map = {
       "SP":0, "LCL":1, "ARG":2, "THIS":3, "THAT":4,
       "R0":0, "R1":1, "R2":2, "R3":3, "R4":4,"R5":5, "R6":6, "R7":7, "R8":8, 
-      "R9":9,"R10":10, "R11":11, "R12":12, "R13":13, "R14":14, "R15":15
+      "R9":9,"R10":10, "R11":11, "R12":12, "R13":13, "R14":14, "R15":15,
+      "SCREEN": 16384, "KBD":24576
 
     }
 
@@ -224,6 +225,8 @@ while parser.hasMoreCommands():
         writeline("{:0>16d}".format(int(bin(int(table.GetAddress(line)))[2:])))
       else :
         storePc = storePc + 1
+        print(line, storePc)
+
         table.addEntry(line, storePc)
         writeline("{:0>16d}".format(int(bin(int(table.GetAddress(line)))[2:])))
 
