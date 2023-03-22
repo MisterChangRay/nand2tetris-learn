71da18c0603cdbfb523ccd5910c6c82e9ba75572
chang.ray <misterchangray@hotmail.com>
2023-03-22 16:11:35 +0800

    add vm.parser

---
+++ nand2tetris/projects/07/VM.py
@@ -0,0 +1,34 @@
+class Parser:
+	def hasMoreCommands():
+		return
+	def advance():
+		return
+	def commandType():
+		return
+	def arg1():
+		return
+	def arg2():
+		return
+	def __init__(self):
+		return
+
+class CodeWriter:
+	def setFileName():
+		return
+	def writeArthmetic():
+		return
+	def writePushPop():
+		return
+
+	def __init__(self):
+		return
+
+def sanfiles():
+	return
+
+
+def start():
+	return
+
+
+start()
