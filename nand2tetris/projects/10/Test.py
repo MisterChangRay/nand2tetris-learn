f207dd6bce46099069b166979559f12224e6dd42
chang.ray <misterchangray@hotmail.com>
2023-04-06 18:05:07 +0800

    写不动了,换一个z方式

--- nand2tetris/projects/10/Test.py
+++ nand2tetris/projects/10/Test.py
@@ -1,11 +1,15 @@
 def spamrun(fn):
     def sayspam(*args):
         print("spam,spam,spam")
+        print(args[0], args[1])
         fn(*args)
     return sayspam
+
 @spamrun
 def useful(a,b):
     print(a*b)
    
-if __name__ == "__main__"
-    useful(2,5) 
+if __name__ == "__main__":
+    for i in range(5):
+    	print(i)
+    	i+=1
