5335da1d95e334c31e0abebb8c3ee2146c2ea3b2
chang.ray <misterchangray@hotmail.com>
2023-04-13 14:36:56 +0800

    add symboltable

--- nand2tetris/projects/11/CompilationEngine.py
+++ nand2tetris/projects/11/CompilationEngine.py
@@ -7,6 +7,41 @@ from JackTokenizer import Token
 import traceback
 from pathlib import Path
 
+class SymbolTable:
+	def add(self, name, dataType, type):
+		if(self.hasSymbol(name)):
+			raise ValueError(f"compilation type error! has more definetion " + name)
+		self.tables[name] = Symbol(name, dataType, type, self.countType(type))
+
+	def countType(self, type):
+		count = 0
+		for k in self.tables:
+			if(k.type == type):
+				count += 1
+		return count
+
+	def hasSymbol(self, name):
+		res = self.tables.get(name)
+		if(res == None):
+			return False
+		return True
+
+	def __init__(self, parent):
+		self.tables = dict()
+		self.next = None
+		self.parent = None
+		if(parent != None):
+			self.parent  = parent
+			parent.next = self
+		pass
+class Symbol:
+	def __init__(self, name, dataType, type, index):
+		self.name = name
+		self.dataType = dataType
+		self.type = type
+		self.index = index
+		pass
+	
 
 def CompilationEngine(filepath):
 	def tag_indent(tagName, expand_none = False):
@@ -492,3 +527,4 @@ if __name__ == "__main__":
 
 	srouceFileOrDir  =sys.argv[1]
 	start(srouceFileOrDir)
+
